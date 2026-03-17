cc5ad1818f14ba7e08d5c66148048fdd4dd83b7c
Andrea Difino <adifino13@gmail.com>
2026-03-17 20:31:04 +0100

    Sistemato il router per trees

--- backend/app/presentation/api/v1/trees/router.py
+++ backend/app/presentation/api/v1/trees/router.py
@@ -1,10 +1,28 @@
-from fastapi import APIRouter
+# app/presentation/api/v1/health/router.py
+
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
 
 from app.application.state import AppState
 
-router = APIRouter(prefix="/trees", tags=["trees"])
+router = InferringRouter(prefix="/trees", tags=["trees"])
 
 
-@router.get("/", status_code=200)
-async def get_trees():
-    return [{"id": t.get_id, "title": t.get_title} for t in AppState.trees]
+@cbv(router)
+class TreesController:
+    @router.get("/", status_code=201)
+    async def get_trees(self):
+        return [
+            {
+                "id": t.get_id,
+                "title": t.get_title,
+                "dependencies": t.get_dependencies,
+                "nodes": {
+                    node.get_id: {
+                        "question": node.get_question,
+                    }
+                    for node in t.nodes
+                },
+            }
+            for t in AppState.trees
+        ]
